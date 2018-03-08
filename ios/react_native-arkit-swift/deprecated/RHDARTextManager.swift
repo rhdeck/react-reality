066008e7d7a49ba4b37b33969173a2a277360342
Ray Deck <ray@raydeck.com>
2018-03-07 19:32:28 -0500

    Removing legacy code

--- ios/react_native-arkit-swift/deprecated/RHDARTextManager.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import SceneKit
-@objc(RHDARTextManager)
-class RHDARTextManager: NSObject {
-    @objc func mount(_ textNode:SCNTextNode, node: SCNNode, frame: String) {
-        node.addChildNode(textNode)
-        RHDARNodeManager.sharedInstance.addNodeToScene(node, inReferenceFrame: frame)
-    }
-}
