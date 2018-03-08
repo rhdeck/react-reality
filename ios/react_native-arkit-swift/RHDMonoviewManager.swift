5be608a4589b980a4102af5e51037fa714cabc19
Ray Deck <ray@raydeck.com>
2018-03-08 10:11:40 -0500

    Cleanup

--- ios/react_native-arkit-swift/RHDMonoviewManager.swift
+++ ios/react_native-arkit-swift/RHDMonoviewManager.swift
@@ -1,5 +1,4 @@
 import Foundation
-//@rn view=RHDMonoView
 @objc(RHDMonoViewManager)
 class RHDMonoviewManager:RCTViewManager {
     var v: RHDMonoview?
@@ -11,7 +10,6 @@ class RHDMonoviewManager:RCTViewManager {
         return false
     }
     @objc func doTap(_ x: Double, y: Double, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-        print("doTap x: " + String(x) + " y: " + String(y))
         guard let v = self.v else { reject("no_view", "No View loaded", nil); return }
         v.handleTap(point: CGPoint(x: CGFloat(x), y: CGFloat(y)), resolve:resolve, reject: reject)
     }
