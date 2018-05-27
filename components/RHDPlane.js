0aa5c6b607f9083332f7aea32eeb1755860919bb
Ray Deck <ray@raydeck.com>
2018-05-27 10:03:15 -0400

    Make plane export a side count (1)

--- components/RHDPlane.js
+++ components/RHDPlane.js
@@ -1,11 +1,15 @@
 import PropTypes from "prop-types";
 import RHDGeometry from "./RHDGeometry";
 import { setPlane } from "../RNSwiftBridge";
-export default RHDGeometry(setPlane, {
-  width: PropTypes.number,
-  height: PropTypes.number,
-  cornerRadius: PropTypes.number,
-  cornerSegmentCount: PropTypes.number,
-  widthSegmentCount: PropTypes.number,
-  heightSegmentCount: PropTypes.number
-});
+export default RHDGeometry(
+  setPlane,
+  {
+    width: PropTypes.number,
+    height: PropTypes.number,
+    cornerRadius: PropTypes.number,
+    cornerSegmentCount: PropTypes.number,
+    widthSegmentCount: PropTypes.number,
+    heightSegmentCount: PropTypes.number
+  },
+  1
+);
