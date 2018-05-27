08e9c619c3cfd55c4b64362ff782763e65aa2bff
Ray Deck <ray@raydeck.com>
2018-05-27 10:02:54 -0400

    Removing and boxing up old code

--- startup.js
+++
@@ -1,7 +0,0 @@
-import { clearScene } from "./RHDSceneManager";
-export default () => {
-  // when reloading the app, the scene should be cleared.
-  // on prod, this usually does not happen, but you can reload the app in develop mode
-  // without clearing, this would result in inconsistency
-  ARKitManager.clearScene();
-};
