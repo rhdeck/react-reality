c09b8602dcc3712180feb2bfba3186a91aaec106
Ray Deck <ray@raydeck.com>
2018-06-03 18:56:03 -0400

    Renaming Animated to AnimatedProvider to make usage more intuitive

--- components/RHDAnimatedProvider.js
+++ components/RHDAnimatedProvider.js
@@ -2,7 +2,7 @@ import React, { createContext, Component } from "react";
 import PropTypes from "prop-types";
 import { setAnimation, removeAnimation } from "../RHDSceneManager";
 const { Provider, Consumer } = createContext({});
-class RHDAnimated extends Component {
+class RHDAnimatedProvider extends Component {
   providerValue = {
     willNativeUpdate: this.willNativeUpdate.bind(this),
     didNativeUpdate: this.didNativeUpdate.bind(this)
@@ -20,13 +20,13 @@ class RHDAnimated extends Component {
     //Do nothing
   }
 }
-RHDAnimated.defaultProps = {
+RHDAnimatedProvider.defaultProps = {
   milliseconds: 250,
   easing: "inout"
 };
-RHDAnimated.propTypes = {
+RHDAnimatedProvider.propTypes = {
   milliseconds: PropTypes.number,
   easing: PropTypes.string
 };
-export { RHDAnimated, Consumer as RHDAnimatedConsumer };
-export default RHDAnimated;
+export { RHDAnimatedProvider, Consumer as RHDAnimatedConsumer };
+export default RHDAnimatedProvider;
