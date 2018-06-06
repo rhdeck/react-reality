b023e259f7ec7bc804213c927f6cc9287003eb1f
Ray Deck <ray@raydeck.com>
2018-06-06 12:07:09 -0400

    Fixing IPD for holokit on iPhone X.

--- RHDDualView.js
+++ RHDDualView.js
@@ -3,41 +3,50 @@ import PropTypes from "prop-types";
 import React, { Component } from "react";
 import RHDPrimaryView from "./RHDPrimaryView";
 import RHDSecondaryView from "./RHDSecondaryView";
+const PixelsPerInch = 458;
+const pixels = 2436;
+const points = 812;
+const PixelsPerPoint = pixels / points;
+const MetersPerInch = 0.0254;
+const MetersPerPixel = MetersPerInch / PixelsPerInch;
+const MetersPerPoint = MetersPerPixel * PixelsPerPoint;
+const PointsPerMeter = 1 / MetersPerPoint;
+const ipd = 0.064;
+const doubleWidth = ipd * PointsPerMeter * 2;
+console.log("hi there!!!", ipd * PointsPerMeter, PixelsPerPoint, PixelsPerInch);
 class RHDDualView extends Component {
   render() {
     return (
-      <SafeAreaView style={{ backgroundColor: "black" }}>
+      <View
+        style={{
+          ...this.props.style,
+          height: "100%",
+          width: "100%",
+          backgroundColor: "red",
+          alignItems: "center"
+        }}
+      >
         <View
           style={{
             ...this.props.style,
+            flexDirection: "row",
             height: "100%",
-            width: "100%",
-            backgroundColor: "black"
+            width: doubleWidth
           }}
         >
-          <View
+          <RHDPrimaryView
+            {...this.props}
             style={{
-              ...this.props.style,
-              flexDirection: "row",
-              paddingHorizontal: 100,
-              height: "100%",
-              width: "100%"
+              flex: 1
             }}
-          >
-            <RHDPrimaryView
-              {...this.props}
-              style={{
-                flex: 1
-              }}
-            />
-            <RHDSecondaryView
-              style={{
-                flex: 1
-              }}
-            />
-          </View>
+          />
+          <RHDSecondaryView
+            style={{
+              flex: 1
+            }}
+          />
         </View>
-      </SafeAreaView>
+      </View>
     );
   }
 }
