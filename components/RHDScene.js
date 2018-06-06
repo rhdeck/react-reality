00b5bb73cbb05ff35aaf258c2a70b537b8b4dec3
Ray Deck <ray@raydeck.com>
2018-06-06 12:05:28 -0400

    Removing extra console log

--- components/RHDScene.js
+++ components/RHDScene.js
@@ -3,7 +3,6 @@ import RHDGeometry from "./RHDGeometry";
 import { setScene } from "../RNSwiftBridge";
 export default RHDGeometry(
   async ({ path }, nodeID) => {
-    console.log("I am going to try to add this path at node", path, nodeID);
     return await setScene(nodeID, path);
   },
   {
