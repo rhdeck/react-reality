e7d3c5024fc46f7bf8bdaf5634abb2a6f12a9435
Ray Deck <ray@raydeck.com>
2018-06-06 12:04:56 -0400

    Adding JS support for importing OBJ models

---
+++ components/RHDModel.js
@@ -0,0 +1,12 @@
+import PropTypes from "prop-types";
+import RHDGeometry from "./RHDGeometry";
+import { setModel } from "../RNSwiftBridge";
+export default RHDGeometry(
+  async ({ path }, nodeID) => {
+    console.log("I am going to try to add this path at node", path, nodeID);
+    return await setModel(nodeID, path);
+  },
+  {
+    path: PropTypes.string
+  }
+);
