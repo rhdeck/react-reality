23b622f06c3921ac1f0ee3f3729419276718ce14
Ray Deck <ray@raydeck.com>
2018-11-01 12:43:08 -0400

    Removing console.log

--- components/ARModel.js
+++ components/ARModel.js
@@ -3,7 +3,6 @@ import ARGeometry from "./ARGeometry";
 import { setModel } from "../RNSwiftBridge";
 export default ARGeometry(
   async ({ path }, nodeID) => {
-    console.log("I am going to try to add this path at node", path, nodeID);
     return await setModel(nodeID, path);
   },
   {
